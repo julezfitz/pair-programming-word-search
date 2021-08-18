f99abbb112b8f90bc7814767334f98137fe0a439
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-18 20:01:26 +0000

    This commit solved word search vertically and reverse horizontally in collaboration with Matt Parisien

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,7 +31,63 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'L', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'L', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'O', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO');
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([
+      [],
+    ], 'HELLO');
+
+    assert.isFalse(result);
+  });
+
+  it("should return false if no word is provided", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'E', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'L', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'L', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'O', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present in reverse horizontally", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['O', 'L', 'L', 'E', 'H', 'V', 'R', 'G'],
+      ['W', 'L', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'L', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'O', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HELLO');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,48 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if ((!word) || (letters.length === -1)) {
+    return false;
+  }
 
-module.exports = wordSearch
+  if (!flatten(letters, word)) {
+    if (!flatten(transpose(letters), word)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = function(matrix, word) {
+  const horizontalJoin = matrix.map(ls => ls.join(''));
+  const reverseJoin = matrix.map(ls => ls.reverse().join(""));
+  console.log(reverseJoin);
+  console.log(horizontalJoin);
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+    for (let i of reverseJoin) {
+      if (i.includes(word)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const transpose = function(matrix) {
+
+  let newMatrix = [];
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix[i] = [];
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      newMatrix[j][i] = matrix[i][j];
+    }
+  }
+  return newMatrix;
+};
+
+module.exports = wordSearch;
